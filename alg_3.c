664e7f9bb07c93c9b99eacc2b5bfcca2bbeae119
idogazit <68930972+idogazit@users.noreply.github.com>
2020-08-28 17:03:30 +0300

    Add files via upload
    some changes

--- alg_3.c
+++ alg_3.c
@@ -25,40 +25,31 @@ double Epsilon = 0.00001;
 
 Group* alg3(Graph_A* graph);
 Group trivial_group(Graph_A* graph);
-void outout_groups(Group* O, int num_groups, char* out_file);
+void output_groups(Group* O, int num_groups, char* out_file);
 
 Group* alg3(Graph_A* graph)
 {
 	Group triv_g = trivial_group(graph);
-	Group* P = calloc(graph->num_nodes, sizeof(int));
-	Group* O;
+	Group* P = calloc(graph->num_nodes, sizeof(Group));
+	Group* O = calloc(graph->num_nodes, sizeof(Group));
 	Group g;
 	Devision temp;
-	int size_P = 1, last_ind_P = 1;
-	int size_O = 0, last_ind_O = 0;
+	int size_P = 1;
+	int size_O = 0;
 	P[0] = triv_g;
-	O = calloc(graph->num_nodes, sizeof(Group));
+
 	while (size_P > 0)
 	{
-		g = P[last_ind_P];
-		--last_ind_P;
+		g = P[size_P];
 		--size_P;
 		
 		/*here comes algorithm 2 into part*/
 		//temp = alg_2(g);
 
-		if (temp.group1.size_g == 0)
+		if (temp.group1.size_g == 0 || temp.group2.size_g == 0)
 		{
-			/*add group2 into O*/
-			O[last_ind_O] = temp.group2;
-			last_ind_O++;
-			size_O++;
-		}
-		if (temp.group2.size_g == 0)
-		{
-			/* add group1 into O*/
-			O[last_ind_O] = temp.group1;
-			last_ind_O++;
+			/*add group g into O*/
+			O[size_O] = g;
 			size_O++;
 		}
 		else
@@ -66,76 +57,78 @@ Group* alg3(Graph_A* graph)
 			if (temp.group1.size_g == 1)
 			{
 				/*add group 1 into O*/
-				O[last_ind_O] = temp.group1;
-				last_ind_O++;
+				O[size_O] = temp.group1;
 				size_O++;
 			}
 			else
 			{
 				/*add group 1 into P*/
-				P[last_ind_P] = temp.group1;
-				last_ind_P++;
+				P[size_P] = temp.group1;
 				size_P++;
 			}
 			if (temp.group2.size_g == 1)
 			{
 				/*add group 2 into O*/
-				O[last_ind_O] = temp.group2;
-				last_ind_O++;
+				O[size_O] = temp.group2;
 				size_O++;
 			}
 			else
 			{
 				/*add group 2 into P*/
-				P[last_ind_P] = temp.group2;
-				last_ind_P++;
+				P[size_P] = temp.group2;
 				size_P++;
 			}
 		}
 	}
+	output_groups(O, size_O, "filename");
 	return O;
 }
 Group trivial_group(Graph_A* graph)
 {
 	Group full;
-	int* arr;
 	int i;
+	
 	full.size_g = graph->num_nodes;
-	arr = calloc(full.size_g, sizeof(int));
+	full.arr_g = calloc(full.size_g, sizeof(int));
 	full.tmp_vec = calloc(full.size_g, sizeof(int));
+
 	for (i = 0; i < full.size_g; i++)
-		arr[i] = i;
-	full.arr_g = arr;
+	{
+		full.arr_g[i] = i + 1;
+	}
+
 	return full;
 }
-void outout_groups(Group* O, int num_groups, char* out_file)
+void output_groups(Group* O, int num_groups, char* out_file)
 {
 	FILE* out_div;
 	int n;
 	Group* pointer;
 	Group g;
 	out_div = fopen(out_file, "w");
+	
 	n = fwrite(&num_groups, sizeof(int), 1, out_div);
 	if (n != 1)
 	{
-		printf("Error writing to output file");
+		printf("Error writing to output file\n");
 	}
+	
 	for (pointer = &O[0]; pointer < &O[num_groups]; pointer++)
 	{
 		g = *pointer;
-		n = fwrite(&g.size_g, sizeof(int), 1, out_div);
+		n = fwrite(&(g.size_g), sizeof(int), 1, out_div);
 		if (n != 1)
 		{
-			printf("Error writing to output file");
+			printf("Error writing to output file\n");
 		}
-		n = fwrite(&g.arr_g, sizeof(int), g.size_g, out_div);
+		n = fwrite(&(g.arr_g), sizeof(int), g.size_g, out_div);
 		if (n != g.size_g)
 		{
-			printf("Error writing to output file");
+			printf("Error writing to output file\n");
 		}
 	}
+	
 	fclose(out_div);
-
 }
 int main(int argc, char* argv[]) {
 	/*Graph_A graph;
